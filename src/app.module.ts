c0192aa5a822073cb6fcac4110b8ed6335328cf8
Вадим Видман <vadimvidman@MacBook-Air-Vadim.local>
2022-11-16 23:26:45 +0400

    add calculator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsModule } from './news/news.module';
+import { CalculatorModule } from './calculator/calculator.module';
 
 @Module({
-  imports: [NewsModule],
+  imports: [NewsModule, CalculatorModule],
   controllers: [AppController],
   providers: [AppService],
 })
